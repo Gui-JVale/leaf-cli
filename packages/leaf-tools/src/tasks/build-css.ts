ed5d69f1fbb9c1eec5d001b3ca54d3a9891f941a
Gui J Vale <gui.vale18@hotmail.com>
2025-04-27 13:07:03 -0700

    fixes css watch bug

--- packages/leaf-tools/src/tasks/build-css.ts
+++ packages/leaf-tools/src/tasks/build-css.ts
@@ -1,10 +1,12 @@
 import path from "path";
+import fs from "fs";
 import gulp from "gulp";
 // import gulpSass from "gulp-sass";
 // import sass from "sass";
 import postcss from "gulp-postcss";
 import plumber from "gulp-plumber";
 import chokidar from "chokidar";
+import glob from "glob";
 
 import { config } from "./includes/config";
 import { messages } from "./includes/messages";
@@ -56,6 +58,24 @@ function processSass(): Promise<NodeJS.ReadWriteStream> {
 }
 
 /**
+ * Touch CSS files to update their modification time
+ * This ensures Shopify's watcher detects the changes
+ */
+function touchCssFiles() {
+  try {
+    const cssFiles = glob.sync(`${config.dist.assets}*.css`);
+    cssFiles.forEach((file) => {
+      // Update the file modification time to trigger Shopify's watcher
+      const now = new Date();
+      fs.utimesSync(file, now, now);
+      messages.logFileEvent("touch", file);
+    });
+  } catch (error) {
+    console.error(`Error touching CSS files:`, error);
+  }
+}
+
+/**
  * Concatenate css via gulp-cssimport
  */
 gulp.task("build:css", () => {
@@ -66,14 +86,28 @@ gulp.task("build:css", () => {
  * Watches css in the `/src` directory
  */
 gulp.task("watch:css", () => {
-  chokidar.watch(assets, { ignoreInitial: true }).on("all", (event, path) => {
-    const isCssFile = /\.s[ac]ss$/i.test(path);
-    // Don't log event twice
-    if (isCssFile) {
-      processSass();
-      messages.logFileEvent(event, path);
-    }
+  console.log("Watching CSS files");
+  chokidar
+    .watch(assets, { ignoreInitial: true })
+    .on("all", async (event, path) => {
+      const isCssFile = /\.s[ac]ss$/i.test(path);
 
-    processCss();
-  });
+      try {
+        // Process the appropriate files
+        if (isCssFile) {
+          await processSass();
+        }
+        await processCss();
+
+        // Important: Touch CSS files after processing to ensure Shopify detects changes
+        setTimeout(() => {
+          touchCssFiles();
+        }, 500);
+
+        // Log the event
+        messages.logFileEvent(event, path);
+      } catch (error) {
+        console.error(`Error processing CSS:`, error);
+      }
+    });
 });
